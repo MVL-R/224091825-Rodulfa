dab0815068d2306c997ed4034ce5971bbe1088a4
MVL-R <rodulfamarkvincentrodulfa@gmail.com>
2024-07-20 11:53:41 +0800

    Batch file opens 5 applications BSCRIPT01.bat

---
+++ BSCRIPT01.bat
@@ -0,0 +1,11 @@
+start https://facebook.com
+start https://youtube.com
+start https://github.com
+start https://messenger.com
+start https://canvas.uc-bcf.edu.ph
+pause
+start calc
+pause
+start notepad
+pause
+shutdown /s
